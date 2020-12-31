ccac9d613d3e6111c973083d0bbd1c407d27747f
airtoxin <airtoxin@icloud.com>
2021-01-01 07:52:09 +0900

    Move move implementation

--- packages/the-rose-king/src/commands.ts
+++ packages/the-rose-king/src/commands.ts
@@ -1,27 +1,83 @@
-import { PowerCard } from "./components";
+import { getNextCrownPosition, PlayableCard } from "./queries";
+import { GameState } from "./game";
+import { GameContext } from "./engine";
+import { Player } from "./player";
+import { GameResult } from "./result";
+import produce from "immer";
+import { random } from "./random";
+import invariant from "tiny-invariant";
+import { assertUnreachable } from "@game/utils";
 
 export type GameCommand = ReturnType<
+  | typeof initCommand
   | typeof moveCommand
-  | typeof moveWithHeroCommand
   | typeof drawCommand
   | typeof passCommand
 >;
 export type GameCommandType = GameCommand["type"];
 
-const moveCommand = (card: PowerCard) => ({
+export const initCommand = () => ({
+  type: "init" as const,
+});
+
+export const moveCommand = (playableCard: PlayableCard) => ({
   type: "move" as const,
-  card,
+  ...playableCard,
 });
 
-const moveWithHeroCommand = (card: PowerCard) => ({
-  type: "moveWithHero" as const,
-  card,
-});
-
-const drawCommand = () => ({
+export const drawCommand = () => ({
   type: "draw" as const,
 });
 
-const passCommand = () => ({
+export const passCommand = () => ({
   type: "pass" as const,
 });
+
+export const move = (
+  state: GameState,
+  command: GameCommand,
+  ctx: GameContext<Player, GameResult>
+): GameState =>
+  produce(state, (draft) => {
+    switch (command.type) {
+      case "init": {
+        break;
+      }
+      case "move": {
+        draft.board.crownPosition = getNextCrownPosition(state, command.card);
+        const draftActivePlayer = draft.players.find(
+          (p) => p.id === ctx.activePlayer.id
+        )!;
+        const draftRow = draft.board.grid[draft.board.crownPosition.y]!;
+        if (command.withHeroCard) {
+          draftRow[draft.board.crownPosition.x]!.color =
+            draftActivePlayer.color;
+          draftActivePlayer.heroCards.pop();
+        } else {
+          draft.numMarkers -= 1;
+          draftRow[draft.board.crownPosition.x] = {
+            id: random.id(),
+            color: draftActivePlayer.color,
+          };
+        }
+        draftActivePlayer.hand = draftActivePlayer.hand.filter(
+          (c) => c.id !== command.card.id
+        );
+        draft.discardPile.push(command.card);
+
+        break;
+      }
+      case "draw": {
+        const card = draft.deck.pop();
+        invariant(card, "No card in deck.");
+        const player = draft.players.find((p) => p.id === ctx.activePlayer.id);
+        invariant(player, "No activePlayer in players");
+        player.hand.push(card);
+        break;
+      }
+      case "pass":
+        break;
+      default:
+        assertUnreachable(command);
+    }
+  });
